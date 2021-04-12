d211869c0d749c71fccf5248155411e815ea7b7a
Egor Badaev <egor.badaev@gmail.com>
2021-04-12 21:48:42 +0300

    Change extension visibility

--- Sources/EBFoundation/UIColorExtensions.swift
+++ Sources/EBFoundation/UIColorExtensions.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-extension UIColor {
+public extension UIColor {
     func image(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
         return UIGraphicsImageRenderer(size: size).image { rendererContext in
             self.setFill()
